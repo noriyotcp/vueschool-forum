3cae3575a59b6da3881eaab042ca5ccd34dadb2c
noriyotcp <noriyo.akita@gmail.com>
2021-01-31 21:01:57 +0900

    Display thread titles, posts and author names in PageHome
    Chapter2: 11

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -2,11 +2,26 @@
   <div>
     Hello from page home
   </div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{ users.find(u => u.id === posts.find(p => p.id === postId).userId).name}}</p>
+      <p>{{ posts.find(p => p.id === postId).text }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
+  import sourceData from "@/data.json";
 
+  export default {
+    data() {
+      return {
+        threads: sourceData.threads,
+        posts: sourceData.posts,
+        users: sourceData.users
+      }
+    }
   }
 </script>
 
