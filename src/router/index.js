2663afb8a344c9f1b366f9fc2391edaaffa30491
noriyotcp <noriyo.akita@gmail.com>
2021-02-13 17:14:21 +0900

    Create PageNotFound.vue for 404 page
    Chapter 3 6.Creating a Not Found Page with Vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import PageHome from '@/components/PageHome';
+import PageNotFound from '@/components/PageNotFound';
 import PageThreadShow from '@/components/PageThreadShow';
 import { createRouter, createWebHistory } from 'vue-router';
 
@@ -13,6 +14,10 @@ const routes = [
     name: 'ThreadShow',
     component: PageThreadShow,
     props: true
+  },
+  { path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: PageNotFound
   }
 ];
 
