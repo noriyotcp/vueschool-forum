0c4e82ea1562a143df252d4f2220ac1b0fd42717
noriyotcp <noriyo.akita@gmail.com>
2021-02-13 16:50:17 +0900

    Navigate between pages via router-link
    Chapter3 5. Using router-link to Navigate Between Page Components

--- src/components/ThreadList.vue
+++ src/components/ThreadList.vue
@@ -11,7 +11,10 @@
         <div class="thread">
           <div>
             <p>
-              <a href="#">{{ thread.title }}</a>
+              <router-link
+                :to="{ name: 'ThreadShow', params: { id: thread.id } }"
+                >{{ thread.title }}</router-link
+              >
             </p>
             <p class="text-faded text-xsmall">
               By <a href="#">{{ userById(thread.userId).name }}</a
