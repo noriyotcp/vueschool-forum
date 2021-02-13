e6e34c9cf7e7433d8956e14c92a8aef6fa7fb12c
noriyotcp <noriyo.akita@gmail.com>
2021-02-13 17:47:06 +0900

    Redirect With Vue Router Without Changing the URL
    Chapter3 7
    
    Redirect to PageNotFound component when page does not exist

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import PageHome from '@/components/PageHome';
 import PageNotFound from '@/components/PageNotFound';
 import PageThreadShow from '@/components/PageThreadShow';
 import { createRouter, createWebHistory } from 'vue-router';
+import sourceData from '@/data.json';
 
 const routes = [
   {
@@ -13,7 +14,22 @@ const routes = [
     path: '/thread/:id',
     name: 'ThreadShow',
     component: PageThreadShow,
-    props: true
+    props: true,
+    beforeEnter (to, from, next) {
+      const threadExists = sourceData.threads.find(thread => thread.id === to.params.id);
+      if (threadExists) {
+        return next();
+      } else {
+        next({
+          name: 'NotFound',
+          // preserve current path and remove the first char to avoid the target URL starting with `//`
+          params: { pathMatch: to.path.substring(1).split('/') },
+          // preserve existing query and hash if any
+          query: to.query,
+          hash: to.hash
+        });
+      }
+    }
   },
   { path: '/:pathMatch(.*)*',
     name: 'NotFound',
