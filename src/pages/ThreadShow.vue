c0327e97abe3ebb29073702e9a91da1c3efd0e14
noriyotcp <noriyo.akita@gmail.com>
2021-02-27 23:02:17 +0900

    24. Creating a Forum Post Component
    Extract posts to PostList.vue
    
    そのスレッドのポストを threadPosts computed property で集めて、Post.vue に posts props として渡す

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -2,49 +2,16 @@
   <h1>{{ thread.title }}</h1>
 
   <post-list :posts="threadPosts" />
-
-  <div class="post-list">
-    <div v-for="postId in thread.posts" :key="postId" class="post">
-      <div class="user-info">
-        <a href="#" class="user-name">{{
-          userById(postById(postId).userId).name
-        }}</a>
-        <a href="#">
-          <img
-            class="avatar-large"
-            :src="userById(postById(postId).userId).avatar"
-            alt=""
-          />
-        </a>
-        <p class="desktop-only text-small">107 posts</p>
-      </div>
-
-      <div class="post-content">
-        <div>
-          <p>
-            {{ postById(postId).text }}
-          </p>
-        </div>
-        <a
-          href="#"
-          style="margin-left: auto"
-          class="link-unstyled"
-          title="Make a change"
-          ><i class="fa fa-pencil"></i
-        ></a>
-      </div>
-
-      <div class="post-date text-faded">
-        {{ postById(postId).publishedAt }}
-      </div>
-    </div>
-  </div>
 </template>
 
 <script>
 import sourceData from '@/data.json';
+import PostList from '@/components/PostList';
 export default {
   name: 'ThreadShow',
+  components: {
+    PostList
+  },
   props: {
     id: {
       required: true,
@@ -54,8 +21,7 @@ export default {
   data () {
     return {
       threads: sourceData.threads,
-      posts: sourceData.posts,
-      users: sourceData.users
+      posts: sourceData.posts
     };
   },
   computed: {
@@ -64,14 +30,6 @@ export default {
     },
     threadPosts () {
       return this.posts.filter((post) => post.threadId === this.id);
-    }
-  },
-  methods: {
-    postById (postId) {
-      return this.posts.find((p) => p.id === postId);
-    },
-    userById (userId) {
-      return this.users.find((u) => u.id === userId);
     }
   }
 };
