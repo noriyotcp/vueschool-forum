6973a3015c525971a7f0c329d172c5f14e5111b9
noriyotcp <noriyo.akita@gmail.com>
2021-03-06 10:51:38 +0900

    Format post publishedAt with human-readable

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -30,8 +30,8 @@
         ></a>
       </div>
 
-      <div class="post-date text-faded">
-        {{ post.publishedAt }}
+      <div class="post-date text-faded" :title="humanFriendlyDate(post.publishedAt)">
+        {{ diffForHumans(post.publishedAt) }}
       </div>
     </div>
   </div>
@@ -39,6 +39,13 @@
 
 <script>
 import sourceData from '@/data.json';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import localizedDate from 'dayjs/plugin/localizedFormat';
+
+dayjs.extend(relativeTime);
+dayjs.extend(localizedDate);
+
 export default {
   name: 'PostList',
   props: {
@@ -55,6 +62,12 @@ export default {
   methods: {
     userById (userId) {
       return this.users.find((u) => u.id === userId);
+    },
+    diffForHumans (timestamp) {
+      return dayjs.unix(timestamp).fromNow();
+    },
+    humanFriendlyDate (timestamp) {
+      return dayjs.unix(timestamp).format('llll');
     }
   }
 };
